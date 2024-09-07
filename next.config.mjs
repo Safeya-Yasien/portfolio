3ec2c1e32280f4ea9747d2034613369760f58a6e
Safeya-Yasien <bluesky7yas@gmail.com>
2024-09-08 00:13:15 +0300

    update configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 /** @type {import('next').NextConfig} */
-
 const __filename = fileURLToPath(import.meta.url);
-// console.log("file name", __filename);
 const __dirname = path.dirname(__filename);
-// console.log("dir name", __dirname);
 
 const nextConfig = {
   sassOptions: {
