81596a1e422c0ee72e0e7e3c6d7b336c7262b698
Safeya-Yasien <bluesky7yas@gmail.com>
2024-09-07 14:23:55 +0300

    project init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const __filename = fileURLToPath(import.meta.url);
+// console.log("file name", __filename);
+const __dirname = path.dirname(__filename);
+// console.log("dir name", __dirname);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
 export default nextConfig;
