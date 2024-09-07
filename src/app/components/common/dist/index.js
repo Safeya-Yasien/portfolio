f695bbe991089315e85a384c8accb8dde08e0608
Safeya-Yasien <bluesky7yas@gmail.com>
2024-09-08 02:23:06 +0300

    Remove build artifacts from version control

--- src/app/components/common/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Footer = exports.Navbar = void 0;
-var Navbar_1 = require("./Navbar");
-exports.Navbar = Navbar_1["default"];
-var Footer_1 = require("./Footer");
-exports.Footer = Footer_1["default"];
--- src/app/components/portfolio/dist/CustomTitle.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var CustomTitle = function (_a) {
-    var title = _a.title;
-    return (React.createElement("h2", { className: "mb-12 text-white text-2xl text-center uppercase tracking-widest border-b-2 border-blue-600 shadow-custom w-fit mx-auto  p-2" }, title));
-};
-exports["default"] = CustomTitle;
