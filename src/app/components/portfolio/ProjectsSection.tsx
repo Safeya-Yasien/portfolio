fedbab65bcc6b1f3b1933aab0b4c91ba3fadc5c0
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-17 04:34:37 +0300

    update projects

--- src/app/components/portfolio/ProjectsSection.tsx
+++ src/app/components/portfolio/ProjectsSection.tsx
@@ -8,7 +8,7 @@ const ProjectsSection = () => {
     <section id="projects">
       <div className="container">
         <CustomTitle title={"Projects"} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 gap-y-8">
           {projectsData.map((project) => (
             <ProjectCard key={project.id} {...project} />
           ))}
