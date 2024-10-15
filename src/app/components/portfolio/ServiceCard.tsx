814a60ca7819298c80e8b310f2fbde362220248d
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-15 05:08:50 +0300

    update services styles

--- src/app/components/portfolio/ServiceCard.tsx
+++ src/app/components/portfolio/ServiceCard.tsx
@@ -8,12 +8,8 @@ type IServiceProps = {
 const ServiceCard = ({ title, description }: IServiceProps) => {
   return (
     <div className="block md:max-w-sm p-6 max-w-full border border-gray-200 bg-neutral-950 rounded-lg shadow bg-">
-      <h3 className="mb-6 text-2xl tracking-tight text-gray-900 ">
-        {title}
-      </h3>
-      <p className="font-normal text-gray-700 ">
-        {description}
-      </p>
+      <h3 className="mb-6 text-2xl tracking-tight  text-white">{title}</h3>
+      <p className="font-normal  text-gray-400">{description}</p>
     </div>
   );
 };
