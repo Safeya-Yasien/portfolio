0c83b158ad6402234e83892a43a9cb78c4d6ab64
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-19 18:37:17 +0300

    update personal info

--- src/app/data/servicesData.ts
+++ src/app/data/servicesData.ts
@@ -2,21 +2,26 @@ export const servicesData = [
   {
     title: "Web Development",
     description:
-      "We offer cutting-edge web development services to help you build an outstanding online presence.",
+      "Leverage modern web technologies to build fast, secure, and scalable websites that cater to your business goals. From single-page applications to complex platforms, I ensure clean, maintainable code and optimal performance.",
   },
   {
     title: "Web Design",
     description:
-      "Create seamless mobile applications that provide a great user experience across devices.",
+      "Craft visually appealing, user-centric designs that prioritize seamless navigation and exceptional user experiences. I turn your ideas into beautiful, responsive web designs with a focus on accessibility and aesthetics.",
   },
   {
     title: "Responsive Design",
     description:
-      "Design intuitive and user-friendly interfaces that ensure a positive customer experience.",
+      "Ensure your website looks great and functions smoothly across all devices. Using mobile-first development and responsive design principles, I create flexible layouts that adapt to various screen sizes while maintaining usability and performance.",
   },
   {
-    title: "Front-End Frameworks",
+    title: "Front-End Frameworks & Libraries",
     description:
-      "Design intuitive and user-friendly interfaces that ensure a positive customer experience.",
+      "Harness the power of modern front-end frameworks like React, along with state management tools like Redux. I specialize in building scalable, component-based applications that are easy to maintain and extend over time.",
+  },
+  {
+    title: "Interactive UI Development",
+    description:
+      "Focus on creating responsive, visually appealing, and highly interactive UIs using advanced CSS techniques (SCSS) and frameworks like Bootstrap and Tailwind.",
   },
 ];
