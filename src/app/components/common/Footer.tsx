f3303f3032f58613c548dd0eecbbb47cd4f4dbc9
Safeya-Yasien <ssafeya5@gmail.com>
2025-05-07 01:59:50 +0300

    update footer text color

--- src/app/components/common/Footer.tsx
+++ src/app/components/common/Footer.tsx
@@ -5,14 +5,15 @@ const Footer = () => {
   return (
     <footer className=" rounded-lg shadow ">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm  sm:text-center text-gray-400">
-          © 2024 
+        <span className="text-sm  sm:text-center text-gray-300">
+          © 2024
           <Link
             href="https://github.com/Safeya-Yasien/"
             target="_blank"
-            className="hover:underline"
+            className="hover:underline hover:text-white"
           >
-            {" "} Safeya Yasien™
+            {" "}
+            Safeya Yasien™
           </Link>
           . All Rights Reserved.
         </span>
