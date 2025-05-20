a551f320ca8edc4772851a31c072703785e77261
Safeya-Yasien <ssafeya5@gmail.com>
2025-05-20 22:16:11 +0300

    add theme switcher in small screens

--- src/app/components/common/Navbar.tsx
+++ src/app/components/common/Navbar.tsx
@@ -9,6 +9,7 @@ import { navLinks } from "@/app/constants/navLinks";
 import ThemeSwitcher from "../theme/ThemeSwitcher";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,6 +29,10 @@ const Navbar = () => {
             Safeya Yasien
           </span>
         </Link>
+
+        {/* menu bar */}
+        <div className="flex items-center gap-2 md:hidden">
+  <ThemeSwitcher />
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -37,22 +42,11 @@ const Navbar = () => {
           onClick={toggleMenu}
         >
           <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
+          <FontAwesomeIcon icon={faBars} className="w-5 h-5" />
         </button>
+
+        </div>
+
         <div
           className={`w-full md:block md:w-auto ${isOpen ? "" : "hidden"}`}
           id="navbar-default"
--- src/app/components/theme/ThemeSwitcher.tsx
+++ src/app/components/theme/ThemeSwitcher.tsx
@@ -1,11 +1,18 @@
 "  use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
